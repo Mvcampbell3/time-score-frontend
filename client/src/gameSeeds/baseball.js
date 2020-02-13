b4abceb6b857c56a2178885fda39b54e9659cba2
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-13 16:58:48 -0500

    mlb teams seed

---
+++ client/src/gameSeeds/baseball.js
@@ -0,0 +1,36 @@
+import { Game } from '../models/game';
+
+const baseballTeams = new Game('MLB Teams', 'Name Every MLB Team', 'City names will not be counted towards correct answers, only the actual team name', [
+  { display_value: 'Tampa Bay Rays', accepted_values: ['rays', 'devil rays'] },
+  { display_value: 'New York Yankees', accepted_values: ['yankees'] },
+  { display_value: 'Boston Red Sox', accepted_values: ['red sox'] },
+  { display_value: 'Baltimore Orioles', accepted_values: ['orioles'] },
+  { display_value: 'Toronto Blue Jays', accepted_values: ['blue jays', 'jays'] },
+  { display_value: 'Cleveland Indiand', accepted_values: ['indians'] },
+  { display_value: 'Detroit Tigers', accepted_values: ['tigers'] },
+  { display_value: 'Kansas City Royals', accepted_values: ['royals'] },
+  { display_value: 'Chicago White Sox', accepted_values: ['white sox'] },
+  { display_value: 'Minnesota Twins', accepted_values: ['twins'] },
+  { display_value: 'Houston Astros', accepted_values: ['astros', 'cheaters'] },
+  { display_value: 'Los Angeles Angels', accepted_values: ['angels'] },
+  { display_value: 'Oakland Athletics', accepted_values: ['athletics', 'a\'s'] },
+  { display_value: 'Seattle Mariners', accepted_values: ['mariners'] },
+  { display_value: 'Texas Rangers', accepted_values: ['rangers'] },
+  { display_value: 'Atlanta Braves', accepted_values: ['braves'] },
+  { display_value: 'Miami Marlins', accepted_values: ['marlins'] },
+  { display_value: 'New York Mets', accepted_values: ['mets', 'metropolitans'] },
+  { display_value: 'Philadelpia Philles', accepted_values: ['philles'] },
+  { display_value: 'Washington Nationals', accepted_values: ['nationals'] },
+  { display_value: 'Chicago Cubs', accepted_values: ['cubs'] },
+  { display_value: 'Cincinnati Reds', accepted_values: ['reds'] },
+  { display_value: 'Milwaukee Brewers', accepted_values: ['brewers'] },
+  { display_value: 'Pittsburgh Pirates', accepted_values: ['pirates'] },
+  { display_value: 'St. Louis Cardinals', accepted_values: ['cardinals', 'cards'] },
+  { display_value: 'Arizona Diamondbacks', accepted_values: ['diamondbacks'] },
+  { display_value: 'Colorado Rockies', accepted_values: ['rockies'] },
+  { display_value: 'Los Angeles Dodgers', accepted_values: ['dodgers'] },
+  { display_value: 'San Diego Padres', accepted_values: ['padres'] },
+  { display_value: 'San Francisco Giants', accepted_values: ['giants'] },
+])
+
+export default baseballTeams;
--- client/src/models/answer.ts
+++ client/src/models/answer.ts
@@ -1,9 +1,11 @@
 export class Answer {
   public display_value: string;
   public accepted_values: string[];
+  public guessed: boolean;
 
   constructor(display_value: string, accepted_values: string[]) {
     this.display_value = display_value;
-    this.accepted_values = accepted_values
+    this.accepted_values = accepted_values;
+    this.guessed = false;
   }
 }
