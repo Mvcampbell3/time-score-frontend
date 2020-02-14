c0993a139a82adfaa66d558626a88595d0d7c86a
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-13 20:46:36 -0500

    moved game to new game-page component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { GameListComponent } from './components/game-list/game-list.component';
+import { GamePageComponent } from './components/game-page/game-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    GameListComponent
+    GameListComponent,
+    GamePageComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/components/game-list/game-list.component.ts
+++ client/src/app/components/game-list/game-list.component.ts
@@ -1,60 +1,16 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { Game } from '../../../models/game';
-import { Answer } from '../../../models/answer';
-import baseballTeam from '../../../gameSeeds/baseball'
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-game-list',
-  templateUrl: './game-list.component.html',
-  styleUrls: ['./game-list.component.scss']
+  selector: 'app-game-page',
+  templateUrl: './game-page.component.html',
+  styleUrls: ['./game-page.component.scss']
 })
 export class GameListComponent implements OnInit {
-  @ViewChild('gameInput', { static: true }) gameInput: ElementRef;
-  game: Game = baseballTeam;
-  guess: string;
-  timer: any;
-  time: number = 60;
-  scoreGame: number = 0;
-  play: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
-
-  }
-
-  startGame() {
-    this.gameInput.nativeElement.disabled = false;
-    this.timer = setInterval(() => {
-      if (this.time <= 0) {
-        clearInterval(this.timer);
-        this.gameOver()
-      } else {
-        this.time--;
-      }
-    }, 1000)
-  }
-
-  evaluateInput(e) {
-    const answerArr: Answer[] = this.game.answers;
-    answerArr.forEach((item: Answer) => {
-      const rightTeam: boolean = item.checkAnswer(this.guess);
-      console.log(rightTeam)
-      if (rightTeam) {
-        this.clearInput()
-      }
-    })
-  }
-
-  clearInput() {
-    this.guess = '';
-  }
-
-  gameOver() {
-    console.log(this.gameInput)
-    this.gameInput.nativeElement.disabled = true;
-    this.scoreGame = this.game.answers.filter(answer => answer.guessed === true).length;
-    this.play = false;
   }
 
 }
