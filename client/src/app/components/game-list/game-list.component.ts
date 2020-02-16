3fa329ef934c42ad8065c64643894bbd4e809847
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-16 03:35:23 -0500

    bg-animation for game list

--- client/src/app/components/game-list/game-list.component.ts
+++ client/src/app/components/game-list/game-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-game-list',
@@ -11,14 +11,39 @@ export class GameListComponent implements OnInit {
   selected: boolean = false;
   selectedGame: string = '';
 
+  @ViewChild('listbg', { static: false }) listbg: ElementRef;
+
+
+  bgClasses: string[] = ['newBG1', 'newBG2', 'newBG3'];
+  pos: number = 1;
+  timer: any;
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.setBackgroundColor(0);
+    }, 500)
+    this.timer = setInterval(() => {
+      if (this.pos < this.bgClasses.length) {
+        this.setBackgroundColor(this.pos);
+        this.pos++;
+      } else {
+        this.pos = 0;
+        this.setBackgroundColor(this.pos);
+        this.pos++;
+      }
+    }, 6000)
   }
 
   selectGame(gameName) {
     this.selectedGame = gameName;
     this.selected = true;
+  }
+
+  setBackgroundColor(num: number) {
+    this.listbg.nativeElement.classList = 'list-bg';
+    this.listbg.nativeElement.classList.add(this.bgClasses[num]);
   }
 
   returnFromGame() {
