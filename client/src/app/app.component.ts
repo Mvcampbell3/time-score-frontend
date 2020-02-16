e9d718a937269dde311d00b9b26d9899b2b9dd13
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-16 03:39:10 -0500

    placed landing back to landing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,8 +9,8 @@ export class AppComponent {
   @ViewChild('navBurger', { static: true }) navBurger: ElementRef;
   @ViewChild('navMenu', { static: true }) navMenu: ElementRef;
 
-  showLanding: boolean = false;
-  showGameList: boolean = true;
+  showLanding: boolean = true;
+  showGameList: boolean = false;
   showInstructions: boolean = false;
 
   gameListView() {
