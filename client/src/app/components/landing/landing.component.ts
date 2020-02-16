ebf537e7c0de44c2a8c73145b20482a4688aa6b0
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-16 01:12:32 -0500

    landing animation

--- client/src/app/components/landing/landing.component.ts
+++ client/src/app/components/landing/landing.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import baseballTeams from '../../gameSeeds/baseball';
+import footballTeams from '../../gameSeeds/football';
+import presidents from '../../gameSeeds/presidents';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +11,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  @ViewChild('outputAnimation', { static: true }) outputAnimation: ElementRef;
+  @ViewChild('roller1', { static: true }) roller1: ElementRef;
+  @ViewChild('roller2', { static: true }) roller2: ElementRef;
+  @ViewChild('roller3', { static: true }) roller3: ElementRef;
+  @ViewChild('roller4', { static: true }) roller4: ElementRef;
+  @ViewChild('roller5', { static: true }) roller5: ElementRef;
+  @ViewChild('roller6', { static: true }) roller6: ElementRef;
+
+  rollerContent1: string = 'Theodore Roosevelt';
+  rollerContent2: string = 'Tampa Bay Rays';
+  rollerContent3: string = 'Atlant Braves';
+  rollerContent4: string = 'Tampa Bay Buccaneers';
+  rollerContent5: string = 'Kansas City Chiefs';
+  rollerContent6: string = 'Thomas Jefferson';
+
+  presidentArray: string[] = [];
+  mlbArray: string[] = [];
+  nflArray: string[] = [];
+  nameChangeTimer: any;
+
   constructor() { }
 
   ngOnInit() {
+    presidents.answers.forEach(president => this.presidentArray.push(president.display_value));
+    baseballTeams.answers.forEach(team => this.mlbArray.push(team.display_value));
+    footballTeams.answers.forEach(team => this.nflArray.push(team.display_value))
+
+    this.startNameChangeTimer()
+    setTimeout(() => {
+      this.rollerRemoveAddClass('a', 'b')
+
+    }, 800)
   }
 
+  rollerRemoveAddClass(classname1, classname2) {
+    this.roller1.nativeElement.classList.remove(`${classname1}-1`)
+    this.roller1.nativeElement.classList.add(`${classname2}-1`)
+    this.roller2.nativeElement.classList.remove(`${classname1}-2`)
+    this.roller2.nativeElement.classList.add(`${classname2}-2`)
+    this.roller3.nativeElement.classList.remove(`${classname1}-3`)
+    this.roller3.nativeElement.classList.add(`${classname2}-3`)
+    this.roller4.nativeElement.classList.remove(`${classname1}-4`)
+    this.roller4.nativeElement.classList.add(`${classname2}-4`)
+    this.roller5.nativeElement.classList.remove(`${classname1}-5`)
+    this.roller5.nativeElement.classList.add(`${classname2}-5`)
+    this.roller6.nativeElement.classList.remove(`${classname1}-6`)
+    this.roller6.nativeElement.classList.add(`${classname2}-6`)
+  }
+
+  startNameChangeTimer() {
+    this.nameChangeTimer = setInterval(() => {
+      this.rollerContent1 = this.presidentArray[this.randomNumberForArray(this.presidentArray)]
+      this.rollerContent2 = this.mlbArray[this.randomNumberForArray(this.mlbArray)];
+      this.rollerContent3 = this.mlbArray[this.randomNumberForArray(this.mlbArray)];
+      this.rollerContent4 = this.nflArray[this.randomNumberForArray(this.nflArray)];
+      this.rollerContent5 = this.nflArray[this.randomNumberForArray(this.nflArray)];
+      this.rollerContent6 = this.presidentArray[this.randomNumberForArray(this.presidentArray)];
+    }, 2000)
+  }
+
+  randomNumberForArray(array: string[]): number {
+    return Math.floor(Math.random() * array.length);
+  }
 }
