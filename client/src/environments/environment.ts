8647db3d871101dc560b92d8a90ed2c133476ebf
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-28 17:14:15 -0500

    env

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAcAo0ip9WUeDs_LrO0vKt3e54zy7xSnvo",
+    authDomain: "more-testing-firebase.firebaseapp.com",
+    databaseURL: "https://more-testing-firebase.firebaseio.com",
+    projectId: "more-testing-firebase",
+    storageBucket: "more-testing-firebase.appspot.com",
+    messagingSenderId: "478166188438",
+    appId: "1:478166188438:web:2e5e2056a90b6438135db7",
+    measurementId: "G-RXHWGE9E5Y"
+  }
 };
 
 /*
