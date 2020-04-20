860c13f52b2986a911641314b7b96907e71831a4
Mvcampbell3 <mvcampbell3@gmail.com>
2020-04-19 20:12:58 -0400

    params map for game info

--- client/src/app/components/game-info/game-info.component.ts
+++ client/src/app/components/game-info/game-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game-info',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameInfoComponent implements OnInit {
 
-  constructor() { }
+  game_id: string;
+  game_sub: Subscription;
+  game: any;
 
-  ngOnInit() {
+  subscriptions: Subscription = new Subscription;
+  constructor(public db: AngularFireDatabase, private route: ActivatedRoute) {
+
   }
 
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: Params) => {
+      this.game_id = params.params.id;
+      console.log(this.game_id);
+      this.getGameInfo();
+    });
+  }
+
+  getGameInfo() {
+    this.game_sub = this.db.object(`games/${this.game_id}`).valueChanges().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.subscriptions.add(this.game_sub);
+        this.game = data;
+      },
+      (err: any) => {
+        console.log(err);
+        this.game_sub.unsubscribe();
+      }
+    )
+  }
 }
